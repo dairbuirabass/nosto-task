b9592a2947e2a008684c10ce008493e5b8ff30f9
Dair Baidauletov <dair.baidauletov@piimega.fi>
2021-08-03 08:14:17 +0300

    Add discount logic for price and listPrice

--- Observer/Product/Load.php
+++ Observer/Product/Load.php
@@ -2,13 +2,27 @@
 
 namespace My\Nosto\Observer\Product;
 
+use Magento\Customer\Model\Session;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 
 class Load implements ObserverInterface
 {
-    public function __construct()
-    {
+
+    /**
+     * @var Session
+     */
+    protected $customerSession;
+
+    /**
+     * Constructor for Load
+     *
+     * @param Session $session
+     */
+    public function __construct(
+        Session $session
+    ) {
+        $this->customerSession = $session;
     }
 
     /**
@@ -19,59 +33,12 @@ class Load implements ObserverInterface
      */
     public function execute(Observer $observer)
     {
-        /* @var $product \NostoProduct */
+        /* @var $product \Nosto\Model\Product\Product */
         $product = $observer->getProduct();
-        $product->setPrice(10);
 
-        /*
-        * Here you can modify any product property you need and it will
-        * automatically be used both in the store front tagging and the
-        * server-to-server API calls.
-        *
-        * Note that any new properties will not automatically be used.
-        *
-        * @see https://support.nosto.com/get-started/tagging-product-pages/
-        */
-
-        /*
-         * Example 1
-         *
-         * Adding "tag2" and "tag3" properties.
-         *
-         * These properties are used to include more information about the
-         * product that can in turn be used for more granular control in the
-         * product recommendations.
-         *
-         * It is important to notice that the `addTag()` function receives a string as an argument
-         * and can be called multiple times in order to add multiple arguments.
-         *  If you call:
-         *  $product->addTag3('example1');
-         *  $product->addTag3('example2');
-         *  You will have `example1` and `example2` as in the tag3 field.
-         *
-         * The `setTag` function takes an array of strings as argument and
-         * will override any previously assigned values to the tag:
-         *
-         * $product->setTag2(['example1', 'example2']);
-         * $product->setTag3(['example3']);
-         *
-         */
-
-        /*
-         * Example 2
-         *
-         * Changing the product image URL.
-         *
-         * If you are, for example, using a 3rd party cloud storage for your
-         * product images.
-         *
-         * The url must be absolute and includes the protocol (http or https).
-         * $product->setImageUrl('http://example.com/p/example.jpg');
-         *
-         */
-
-        // This can be removed when implementing this extension, it's here only to
-        // check that the override works when implemented.
-        $product->addTag3('nosto');
+        if ($this->customerSession->isLoggedIn()) {
+            $product->setPrice($product->getPrice() * 0.9);
+            $product->setListPrice($product->getListPrice() * 0.9);
+        }
     }
 }
